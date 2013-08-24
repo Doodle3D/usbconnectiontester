003b44908d767dd12ad7c19d023ba9b9d93c0414
peteruithoven <peter@peteruithoven.nl>
2013-08-24 03:16:16 +0200

    New c code

--- src/usbconnectiontester.c
+++ src/usbconnectiontester.c
@@ -1,51 +1,37 @@
-#define _GNU_SOURCE
-#define __USE_XOPEN_EXTENDED
-#include <unistd.h>
+/*
+ * USB serial connectivity tester
+ *
+ * based on: http://lists.uclibc.org/pipermail/uclibc/2008-January/039683.html
+ * see: http://marc.info/?l=linux-serial&m=120661887111805&w=2
+ *
+ * TODO: Add a 250000 hack
+ */
+#include <errno.h>
 #include <fcntl.h>
+#include <inttypes.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
-#include <stdlib.h>
-#include <time.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/select.h>
-#include <dirent.h>
-
-//#define ENABLE_B300_HACK 1 /* see: http://www.panix.com/~grante/files/arbitrary-baud.c for termios2 usage */
-//#define DEFAULT_TO_115K2 1
+#include <unistd.h>
 
 #include <linux/serial.h>
-#ifdef ENABLE_B300_HACK
-#include <sys/ioctl.h>
-#include <termios.h>
-#else
 #include <linux/termios.h>
-#endif
+//#include <sys/ioctl.h>
 
-#define BASE_PATH "/tmp/UltiFi"
-
+static char* portname;
+static int baud_rate;
+int currentSpeed;
+static int serial_fd;
 int lineBufferPos = 0;
 char lineBuffer[1024];
-int serialfd;
-int tempRecieveTimeout = 2; // how many times is a temperature timeout allowed (after temperature is received this should be more)
-int temperatureCheckDelay = 30; //50
-int tryAlternativeSpeed = 1;
-int currentSpeed;
 
-char basePath[128];
-char temperatureFilename[128];
-char sdlistFilename[128];
-char endstopFilename[128];
-char printProgressFilename[128];
-char printProgress2Filename[128];
-char commandFilename[128];
-char startLogFilename[128];
-//char serverLogFilename[128];
-FILE* commandFile = NULL;
-FILE* sdlistFile = NULL;
-FILE* startLog = NULL;
+typedef enum SET_SPEED_RESULT {
+	SSR_OK = 0, SSR_IO_GET, SSR_IO_SET, SSR_IO_MGET, SSR_IO_MSET1, SSR_IO_MSET2
+} SET_SPEED_RESULT;
+
+int checkDelay = 5;
+int recieveTimeout = 2;
 
 enum LOG_LEVELS {
 	QUIET = -1,
@@ -56,13 +42,8 @@ enum LOG_LEVELS {
 	BULK = 4
 };
 
-int serverLogLevel = WARNING; //default value, this is probably overridden in main()
+int serverLogLevel = BULK; //default value, this is probably overridden in main()
 FILE* serverLogFile = NULL;
-
-/* GCode streaming code */
-FILE* gcodeFile = NULL;
-int gcodeLineNr;
-int gcodeNumLines;
 
 void logger(int level, const char* format, ...) {
 	va_list args;
@@ -80,214 +61,18 @@ void logger(int level, const char* format, ...) {
 	va_end(args);
 }
 
-void sendSingleLine(const char* gcode) {
-	logger(VERBOSE, "send single line: '%s'\n", gcode);
-	write(serialfd, gcode, strlen(gcode));
-}
-
-void sendGCodeLineWithChecksum(const char* gcode)
-{
-	char buffer[128];
-	unsigned char checksum = 0;
-	int n = 0;
-	sprintf(buffer, "N%d%s", gcodeLineNr, gcode);
-	while(buffer[n])
-		checksum ^= buffer[n++];
-	sprintf(buffer, "N%d%s*%d\n", gcodeLineNr, gcode, checksum);
-	logger(BULK, "GCODE: %s", buffer);
-	write(serialfd, buffer, strlen(buffer));
-	gcodeLineNr++;
-
-	FILE* printProgress2File = fopen(printProgress2Filename, "w");
-	fprintf(printProgress2File, "%d/%d\n", gcodeLineNr,gcodeNumLines);
-	fclose(printProgress2File);
-}
-
-void sendNextGCodeLine()
-{
-	char lineBuffer[128];
-	char* c;
-	if (!fgets(lineBuffer, sizeof(lineBuffer), gcodeFile))
-	{
-		cancelCodeFile();
-		return;
-	}
-	c = strchr(lineBuffer, ';');
-	if (c)
-		*c = '\0';
-	c = lineBuffer + strlen(lineBuffer) - 1;
-	while(c >= lineBuffer && c[0] <= ' ')
-	{
-		c[0] = '\0';
-		c--;
-	}
-	//logger(BULK, "gcode %03ib: '%s'\n", strlen(lineBuffer), lineBuffer); //TEMP
-	if (lineBuffer[0] == '\0')
-		sendNextGCodeLine();
-	else
-		sendGCodeLineWithChecksum(lineBuffer);
-}
-
-void startCodeFile(const char* filename)
-{
-	//int i;
-	gcodeFile = fopen(filename, "r");
-	if (gcodeFile == NULL)
-		return;
-	gcodeLineNr = 0;
-
-	gcodeNumLines = getNumLines(gcodeFile);
-	printf("  gcodeNumLines: %d\n", gcodeNumLines);
-
-	sendGCodeLineWithChecksum("M110");
-	//for(i=0;i<6;i++)
-		sendNextGCodeLine();
-}
-
-int getNumLines(fileName) {
-	int ch, numLines = 0;
-	do {
-	    ch = fgetc(fileName);
-	    if(ch == '\n')
-	    	numLines++;
-	} while (ch != EOF);
-	// last line doesn't end with a new line!
-	// but there has to be a line at least before the last line
-	if(ch != '\n' && numLines != 0) 
-	    numLines++;
-	
-	fseek(fileName, 0, SEEK_SET);
-	return numLines;
-}
-
-void cancelCodeFile() {
-	if(gcodeFile != NULL) fclose(gcodeFile);
-	gcodeFile = NULL;
-	printf("finished GCode file");
-
-	FILE* printProgress2File = fopen(printProgress2Filename, "w");
-	fprintf(printProgress2File, "");
-	fclose(printProgress2File);
-}
-
-void parseLine(const char* line)
-{
-	logger(INFO, "|%s|\n", line);
-	if (strstr(line, "Resend:"))
-	{
-		gcodeLineNr = atoi(strstr(line, "Resend:") + 7);
-		return;
-	}
-	if (strcmp(line, "ok") == 0)
-	{
-		if (gcodeFile != NULL)
-		{
-			sendNextGCodeLine();
-		}
-	}
-	if (startLog != NULL)
-	{
-		if (strstr(line, "echo:"))
-		{
-			fprintf(startLog, "%s\n", line);
-		}else{
-			fclose(startLog);
-			startLog = NULL;
-		}
-	}
-	if (strcmp(line, "start") == 0)
-	{
-		startLog = fopen(startLogFilename, "w");
-	}
-	if (strstr(line, "ok T:") || (strstr(line, "T:") == line))
-	{
-		FILE* f = fopen(temperatureFilename, "w");
-		fprintf(f, "%s\n", line);
-		fclose(f);
-		tempRecieveTimeout = 5; // increase timeout when a temperature is received.
-
-		// keep checking the temperature while printing, but less frequently
-		if (gcodeFile == NULL) temperatureCheckDelay = 20;
-		else temperatureCheckDelay = 100;
-		return;
-	}
-	if (strstr(line, "SD printing byte "))
-	{
-		//SD printing byte xxx/xxx
-		//Can report 0/0 when there is no file open.
-		FILE* f = fopen(printProgressFilename, "w");
-		fprintf(f, "%s\n", line);
-		fclose(f);
-		return;
-	}
-	if (strstr(line, "Not SD printing"))
-	{
-		//Only happens when there is no SD card inserted.
-		FILE* f = fopen(printProgressFilename, "w");
-		fclose(f);
-		return;
-	}
-	if (strstr(line, "echo:endstops hit:"))
-	{
-		FILE* f = fopen(endstopFilename, "w");
-		fprintf(f, "%s", line);
-		fclose(f);
-		return;
-	}
-	if (strcmp(line, "echo:SD card ok") == 0)
-	{
-		sendSingleLine("M20\n");
-		return;
-	}
-	if (strcmp(line, "Begin file list") == 0)
-	{
-		sdlistFile = fopen(sdlistFilename, "w");
-		return;
-	}
-	if (strcmp(line, "End file list") == 0)
-	{
-		fclose(sdlistFile);
-		sdlistFile = NULL;
-		return;
-	}
-	if (sdlistFile)
-	{
-		fprintf(sdlistFile, "%s\n", line);
-		return;
-	}
-}
-
-void checkActionDirectory()
-{
-	if (commandFile == NULL)
-		commandFile = fopen(commandFilename, "rb");
-}
-
-void switchSerialSpeed() {
-	int newSpeed = (currentSpeed == 115200)? 250000 : 115200;
-	setSerialSpeed(newSpeed);
-}
-
-void setSerialSpeed(int speed)
-{
-	logger(INFO, "Trying speed: %d\n",speed);
-#ifdef ENABLE_B300_HACK
-	struct termios options;
-#else
+/* based on setSerialSpeed in UltiFi */
+static int setPortSpeed(int fd, int speed) {
 	struct termios2 options;
-#endif
 	int modemBits;
 
-#ifdef ENABLE_B300_HACK
-	tcgetattr(serialfd, &options);
-#else
-	ioctl(serialfd, TCGETS2, &options);
-#endif
+	if (ioctl(fd, TCGETS2, &options) < 0) return SSR_IO_GET;
 
 	cfmakeraw(&options);
 
 	// Enable the receiver
 	options.c_cflag |= CREAD;
+
 	// Clear handshake, parity, stopbits and size
 	options.c_cflag &= ~CLOCAL;
 	options.c_cflag &= ~CRTSCTS;
@@ -295,203 +80,142 @@ void setSerialSpeed(int speed)
 	options.c_cflag &= ~CSTOPB;
 	options.c_cflag &= ~CSIZE;
 
-	switch(speed)
-	{
-	case 38400:
-		cfsetispeed(&options, B38400);
-		cfsetospeed(&options, B38400);
-		break;
-	case 57600:
-		cfsetispeed(&options, B57600);
-		cfsetospeed(&options, B57600);
-		break;
-	case 115200:
-		cfsetispeed(&options, B115200);
-		cfsetospeed(&options, B115200);
-		break;
-	case 250000:
-#ifdef ENABLE_B300_HACK
-		//Hacked the kernel to see B300 as 250000
-		cfsetispeed(&options, B300);
-		cfsetospeed(&options, B300);
-#else
-		options.c_ospeed = options.c_ispeed = 250000;
-		options.c_cflag &= ~CBAUD;
-		options.c_cflag |= BOTHER;
-#endif
-		break;
-	}
+	//set speed
+	options.c_ospeed = options.c_ispeed = speed;
+	options.c_cflag &= ~CBAUD;
+	options.c_cflag |= BOTHER;
+
 	options.c_cflag |= CS8;
 	options.c_cflag |= CLOCAL;
 
-#ifdef ENABLE_B300_HACK
-	tcsetattr(serialfd, TCSANOW, &options);
-#else
-	ioctl(serialfd, TCSETS2, &options);
-#endif
+	if (ioctl(fd, TCSETS2, &options) < 0) return SSR_IO_SET;
 
-	ioctl(serialfd, TIOCMGET, &modemBits);
+	//toggle DTR
+	if (ioctl(fd, TIOCMGET, &modemBits) < 0) return SSR_IO_MGET;
 	modemBits |= TIOCM_DTR;
-	ioctl(serialfd, TIOCMSET, &modemBits);
+	if (ioctl(fd, TIOCMSET, &modemBits) < 0) return SSR_IO_MSET1;
 	usleep(100 * 1000);
 	modemBits &=~TIOCM_DTR;
-	ioctl(serialfd, TIOCMSET, &modemBits);
+	if (ioctl(fd, TIOCMSET, &modemBits) < 0) return SSR_IO_MSET2;
 
 	currentSpeed = speed;
+
+	return SSR_OK;
 }
 
-int main(int argc, char** argv)
-{
-	const char* portName = NULL;
+void send(const char* code) {
+	logger(INFO,"send: '%s'\n", code);
+	write(serial_fd, code, strlen(code));
+}
+void parseLine(const char* line) {
+	logger(INFO, "|%s|\n", line);
+	recieveTimeout	= 2;
+}
+int readSerial() {
+	logger(BULK,"  Checking\n");
+	fd_set fdset;
+	struct timeval timeout;
 
-	if (argc > 1)
-		portName = argv[1];
+	FD_ZERO(&fdset);
+	FD_SET(serial_fd, &fdset);
 
-	if (portName == NULL)
-		portName = "/dev/ttyACM0";
+	timeout.tv_sec = 0;
+	timeout.tv_usec = 100 * 1000;//100ms timeout
+	select(serial_fd + 1, &fdset, NULL, NULL, &timeout);
 
-	sprintf(basePath, "%s/%s", BASE_PATH, strrchr(portName, '/') + 1);
-	mkdir(BASE_PATH, 0777);
-	mkdir(basePath, 0777);
-	sprintf(temperatureFilename, "%s/temp.out", basePath);
-	sprintf(sdlistFilename, "%s/sdlist.out", basePath);
-	sprintf(endstopFilename, "%s/endstop.out", basePath);
-	sprintf(printProgressFilename, "%s/progress.out", basePath);
-	sprintf(printProgress2Filename, "%s/progress2.out", basePath);
-	sprintf(commandFilename, "%s/command.in", basePath);
-	sprintf(startLogFilename, "%s/startup.out", basePath);
-	//sprintf(serverLogFilename, "%s/server.log", basePath);
+	if (FD_ISSET(serial_fd, &fdset)) {
+		logger(BULK,"  Receiving\n");
+		int len = read(serial_fd, lineBuffer + lineBufferPos, sizeof(lineBuffer) - lineBufferPos - 1);
+		if (len < 1) {
+			logger(INFO, "Connection closed.\n");
+			exit(EXIT_FAILURE);
+			return;
+		}
+		lineBufferPos += len;
+		lineBuffer[lineBufferPos] = '\0';
+		while(strchr(lineBuffer, '\n'))	{
+			char* c = strchr(lineBuffer, '\n');
+			*c++ = '\0';
+			parseLine(lineBuffer);
+			lineBufferPos -= strlen(lineBuffer) + 1;
+			memmove(lineBuffer, c, strlen(c) + 1);
+		}
+		//On buffer overflow clear the buffer (this usually happens on the wrong baudrate)
+		if (lineBufferPos == sizeof(lineBuffer) - 1) {
+			lineBufferPos = 0;
+		}
+	}
+}
 
-	//sprintf(lineBuffer, "stty -F %s raw 115200 -echo -hupcl", portName);
-	//system(lineBuffer);
+int main(int argc, char** argv) {
+	SET_SPEED_RESULT spdResult;
 
 	//setup logger
-	serverLogLevel = BULK;
 	serverLogFile = stderr;
 
-	serialfd = open(portName, O_RDWR | O_NONBLOCK );
-	if (serialfd == -1)  {
-        logger(ERROR,"Unable to open serial port\n");
-        return -1;
-    }
+	//if (argc < 2) {
+	//	fprintf(stderr, "%s: please supply a port name, optionally followed by the port speed\n", argv[0]);
+	//	exit(1);
+	//}
 
-#ifndef DEFAULT_TO_115K2
-	logger(INFO, "Setting port speed to 250000\n");
-	setSerialSpeed(250000);
-#else
-	logger(INFO, "Setting port speed to 115200\n");
-	setSerialSpeed(115200);
-#endif
-
-	logger(INFO, "Start\n");
-	while(1)
-	{
-		fd_set fdset;
-		struct timeval timeout;
-
-		FD_ZERO(&fdset);
-		FD_SET(serialfd, &fdset);
-
-		timeout.tv_sec = 0;
-		timeout.tv_usec = 100 * 1000;//100ms timeout
-		select(serialfd + 1, &fdset, NULL, NULL, &timeout);
-		if (FD_ISSET(serialfd, &fdset))
-		{
-			//logger(BULK,"FD_ISSET\n");
-			int len = read(serialfd, lineBuffer + lineBufferPos, sizeof(lineBuffer) - lineBufferPos - 1);
-			if (len < 1)
-			{
-				logger(INFO, "Connection closed.\n");
-				break;
-			}
-			lineBufferPos += len;
-			lineBuffer[lineBufferPos] = '\0';
-			while(strchr(lineBuffer, '\n'))
-			{
-				char* c = strchr(lineBuffer, '\n');
-				*c++ = '\0';
-				parseLine(lineBuffer);
-				lineBufferPos -= strlen(lineBuffer) + 1;
-				memmove(lineBuffer, c, strlen(c) + 1);
-			}
-			//On buffer overflow clear the buffer (this usually happens on the wrong baudrate)
-			if (lineBufferPos == sizeof(lineBuffer) - 1)
-				lineBufferPos = 0;
-		}
-		else
-		{
-			//logger(BULK,"no FD_ISSET\n");
-			//if(gcodeFile == NULL) {
-				if (temperatureCheckDelay > 0)
-				{
-					temperatureCheckDelay--;
-				}
-				else
-				{
-					temperatureCheckDelay = 30; //100;
-					sendSingleLine("M105\n");
-					logger(BULK," tempRecieveTimeout: %d\n",tempRecieveTimeout);
-					if (tempRecieveTimeout)
-					{
-						tempRecieveTimeout--;
-					}
-					else
-					{
-						if (tryAlternativeSpeed)
-						{
-							logger(WARNING, "Failed to connect, let's try different speed\n");
-
-							switchSerialSpeed();
-							logger(BULK, "Connection switched speeds\n");
-
-							lineBufferPos = 0;
-							//tryAlternativeSpeed = 0;
-							tempRecieveTimeout = 2;
-						}
-						else
-						{
-							logger(WARNING, "Failed to connect\n");
-							break;
-						}
-					}
-				}
-			//}
-		}
-		if (commandFile != NULL)
-		{
-			logger(BULK, "commandFile found\n");
-			char sendLine[1024];
-			if (fgets(sendLine, sizeof(sendLine), commandFile) != NULL)
-			{
-				if (strstr(sendLine, "(SENDFILE=") == sendLine) 
-				{ //ptr==.ptr
-					if (sendLine[strlen(sendLine)-1]==10) sendLine[strlen(sendLine)-1] = 0; //recplace LF by \0
-					logger(INFO, "sending '%s'\n",sendLine+sizeof("SENDFILE="));
-					startCodeFile(sendLine+sizeof("SENDFILE=")); //remaining characters after (SENDFILE=
-				} 
-				else 
-				{
-					sendSingleLine(sendLine);
-				}
-
-				if (strstr(sendLine, "(CANCELFILE") == sendLine) 
-				{ //ptr==.ptr
-					fclose(gcodeFile);
-					gcodeFile = NULL;
-					logger(INFO, "cancelled GCode file\n");
-				}
-			}
-			else
-			{
-				logger(BULK, "  commandFile empty\n");
-				//sendSingleLine("\n"); // why would we send a empty line?
-				fclose(commandFile);
-				unlink(commandFilename);
-				commandFile = NULL;
-			}
-		}
-		checkActionDirectory();
+	if (argc >= 2) {
+		serverLogLevel = strtol(argv[1], NULL, 10);
+	} else {
+		serverLogLevel = VERBOSE;
 	}
-	close(serialfd);
-	return 0;
+
+	if (argc >= 3) {
+		portname = argv[2];
+	} else {
+		portname = "/dev/ttyACM0";
+	}
+
+	if (argc >= 4) {
+		baud_rate = strtol(argv[5], NULL, 10);
+	} else {
+		baud_rate = 115200;
+	}
+
+	serial_fd = open(portname, O_RDWR);
+	if (serial_fd == -1) {
+		logger(ERROR, "%s: could not open port %s (%s)\n", argv[0], portname, strerror(errno));
+		exit(2);
+	}
+
+	logger(INFO,"using port %s with speed %i\n", portname, baud_rate);
+
+	spdResult = setPortSpeed(serial_fd,baud_rate);
+	switch (spdResult) {
+		case SSR_OK:
+			logger(INFO, "port opened ok\n");
+			break;
+		case SSR_IO_GET: logger(ERROR, "ioctl error in setPortSpeed() on TCGETS2 (%s)\n", strerror(errno));
+		case SSR_IO_SET: logger(ERROR, "ioctl error in setPortSpeed() on TCSETS2 (%s)\n", strerror(errno));
+		case SSR_IO_MGET: logger(ERROR, "ioctl error in setPortSpeed() on TIOCMGET (%s)\n", strerror(errno));
+		case SSR_IO_MSET1: logger(ERROR, "ioctl error in setPortSpeed() on TIOCMSET1 (%s)\n", strerror(errno));
+		case SSR_IO_MSET2: logger(ERROR, "ioctl error in setPortSpeed() on TIOCMSET2 (%s)\n", strerror(errno));
+			exit(1);
+			break;
+	}
+
+	while(1) {
+		readSerial();
+
+		if (checkDelay > 0) {
+			checkDelay--;
+		} else {
+			send("M105\n");
+			logger(VERBOSE, "new recieveTimeout: %d\n",recieveTimeout);
+			if (recieveTimeout) {
+				recieveTimeout--;
+			} else {
+				logger(ERROR, "Failed to connect\n");
+				exit(1);
+			}
+
+			checkDelay = 5;
+		}
+	}
+	close(serial_fd);
+	exit(0);
 }
